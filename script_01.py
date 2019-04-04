b40c97148a080f72c818529b9a1c3b9f289db341
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2019-04-04 15:32:37 +0200

    Second commit

--- script_01.py
+++ script_01.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 
+import argparse
+
+parser = argparse.ArgumentParser(description='An example python script, ready for extensions.')
+
+args = parser.parse_args()
+
 print("Hello, World!")
