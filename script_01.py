f390e312d3eadb677d3a09bf22997b088947171d
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2019-04-10 16:20:44 +0200

    Added Jens arguments

--- script_01.py
+++ script_01.py
@@ -3,7 +3,11 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='An example python script, ready for extensions.')
+parser.add_argument('--jens', action = 'store_true')
 
 args = parser.parse_args()
 
 print("Hello, World!")
+
+if(args.jens):
+    print("Hello, Jens!")
