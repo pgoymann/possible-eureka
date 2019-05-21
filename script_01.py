33cc1fed8e0f0daeec3f22ee75cb857604264206
Schultheis <hschult@kerckhoff.mpg.de>
2019-05-21 10:56:52 +0200

    added hendrik arg

--- script_01.py
+++ script_01.py
@@ -4,10 +4,13 @@ import argparse
 
 parser = argparse.ArgumentParser(description='An example python script, ready for extensions.')
 parser.add_argument('--jens', action = 'store_true')
-
+parser.add_argument('--hendrik', action = 'store_true')
 args = parser.parse_args()
 
 print("Hello, Loosolab!")
 
 if(args.jens):
     print("Hello, Jens!")
+
+if(args.hendrik):
+    print("Hello, Hendrik!")
