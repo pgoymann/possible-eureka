2b5c0f981862b4046f1df89945984d0cf19eca0c
msbentsen <mette.bentsen@mpi-bn.mpg.de>
2019-05-21 10:51:30 +0200

    added name option

--- script_01.py
+++ script_01.py
@@ -4,6 +4,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='An example python script, ready for extensions.')
 parser.add_argument('--jens', action = 'store_true')
+parser.add_argument('--name')
 
 args = parser.parse_args()
 
@@ -11,3 +12,6 @@ print("Hello, Loosolab!")
 
 if(args.jens):
     print("Hello, Jens!")
+
+if (args.name):
+	print("Hello, {0}!".format(args.name))
